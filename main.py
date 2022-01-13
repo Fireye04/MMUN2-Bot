ad31ad80671d879599226ca49a5f38c09ac6a172
Fireye <45439844+Fireye04@users.noreply.github.com>
2022-01-13 01:04:04 +0000

    init (better)

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import discord
-
-
-from discord.ext import commands    
+import pickle
+import asyncio
+from discord.ext import commands 
 client = commands.Bot(command_prefix=".")
 client.remove_command('help')
 
+@client.event
+async def on_ready():
+    print("Ready")
 
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
 
+@client.command(aliases=["m"])
+async def message(ctx):
+    await ctx.send("It worked!")
 
-
-
-client.run("NzYzMjQ5NTIzODYxNjE4Njg4.X309gg.YdUJjO3eTHqSeK19mBqoq5Y-Ok0")
+client.run("")
