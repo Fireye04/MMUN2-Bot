26193a55515af0fc0ed41ba9fbc8fbc509b3d913
Fireye <45439844+Fireye04@users.noreply.github.com>
2022-01-13 00:52:07 +0000

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+
+
+from discord.ext import commands    
+client = commands.Bot(command_prefix=".")
+client.remove_command('help')
+
+
+
+
+
+
+client.run("NzYzMjQ5NTIzODYxNjE4Njg4.X309gg.YdUJjO3eTHqSeK19mBqoq5Y-Ok0")
