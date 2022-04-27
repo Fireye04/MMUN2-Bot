71e4b16765a4249949b709f101086a293ac2daca
Fireye <45439844+Fireye04@users.noreply.github.com>
2022-04-27 15:12:16 +0000

    changes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import discord
+simport discord
 import pickle
 import asyncio
 import nest_asyncio
 from random import randint
-
+from random import choice
+import re
 
 nest_asyncio.apply()
 from discord.utils import get
@@ -50,7 +51,7 @@ Countries = []
 
 async def aClassInitCategory(ctx, name, self):
 	cat = await ctx.message.guild.create_category(name)
-	await cat.create_text_channel(self.name.lower() + "-general")
+	await cat.create_text_channel(self.channelNames + "-general")
 	return
 
 
@@ -88,7 +89,7 @@ class Country():
 		self.name = name
 		self.managerID = ctx.message.author.id
 		#create country category
-
+		self.channelNames = re.sub('[^A-Za-z0-9 ]+', '', self.name.lower()).replace(" ", "-")
 		
 		self.categoryName = name
 		asyncio.run(aClassInitCategory(ctx, name, self))
@@ -103,7 +104,7 @@ class Country():
 		
 		chair = get(ctx.message.guild.categories, name='chair')
 		
-		self.chairChannelName = name.lower() + '-chair'
+		self.chairChannelName = self.channelNames
 		asyncio.run(aClassInitChairChannel(ctx, name, chair, self))
 		
 		asyncio.run(aClassInitChairPerms(ctx, name, self))
@@ -125,45 +126,103 @@ class Country():
 		#Tech
 		#Awunga bunga
 
-		self.population = randint(1,10)
 
-		self.education = randint(1,10)
+		""" UPDATED
+		land size
+		population
+		education
+		nationalism
+		lumber, ore, uranium
+		wealth, income, food
+		standard of living
+		army (specifics TBD)
+		tech
+		"""
+		#8
+		stats = [1, 2, 4, 5, 5, 5, 8, 10]
 
-		self.nationalism = randint(1,10)
+		def yoink(stat):
+			global stats
+			e = choice(stat)
+			stats = stat.remove(e)
+			return e
 
-		self.natural_resources = randint(1,10)
+		self.land_size = yoink(stats)
+
+		self.standard_of_living = yoink(stats)
+
+		self.land_lumber = yoink(stats)
+
+		self.land_ore = yoink(stats)
+
+		self.land_uranium = yoink(stats)
+
+		self.land_fertility = yoink(stats)
+
+		self.population = (1000000 * self.land_size) / (self.standard_of_living/2)
+		#NOTE: add birth and death rates later
+		self.education = yoink(stats)
+
+		self.nationalism = yoink(stats)
+
+		self.lumber = 0
+
+		self.ore = 0
+
+		self.uranium = 0
 
 		self.wealth = 10
-
+		#note: come up with equation for income
 		self.income = 0
 
-		self.army_size = 0
-
-		self.combat_power = 0
-
-		self.unit_types = ["Infantry"]
-
-		self.infrastructure = randint(1,10)
+		self.food = 0
 
 		self.tech = [
-			("pottery", False)
+			#also remember commas, dumbass
+			#Name, Is complete?, %of completion
+			("Mine", False, 0),
+			("Lumber Mill", False, 0)
 		]
-		# NOTE TO SELF DELETE COUNTRIES WHEN THEIR OWNER LEAVES THE SERVER
+			
+		#self.army_size = 0
 
+		#self.combat_power = 0
+
+		#self.unit_types = ["Infantry"]
+
+		#self.infrastructure = randint(1,10)
+		#self.tech = [
+		#	("pottery", False)
+		#]
+		# NOTE TO SELF DELETE COUNTRIES WHEN THEIR OWNER LEAVES THE SERVER
+	async def techTree(self, ctx):
+		embedVar = discord.Embed(title=f"{self.name} Tech", description="Country technologies", color= 0xe74c3c)
+		for i, techno in enumerate(self.tech):
+			if techno[2] > 0 and techno[2] < 1:
+				embedVar.add_field(name=techno[0], value="In Progress", inline=False)
+			elif techno[2] == 0:
+				embedVar.add_field(name=techno[0], value="Unresearched", inline=False)
+			elif techno[2] == 1:
+				embedVar.add_field(name=techno[0], value="Completed", inline=False)
+		await ctx.send(embed=embedVar)
+		
 	async def getStats(self, ctx):
 		embedVar = discord.Embed(title=f"{self.name} Stats", description="Country statistics", color= 0xe74c3c)
-		embedVar.add_field(name=str(self.population), value="Population", inline=True)
-		embedVar.add_field(name=str(self.education), value="Education", inline=True)
+		embedVar.add_field(name=str(self.land_size), value="Land Size", inline=True)
+		embedVar.add_field(name=str(self.land_lumber), value="Land Lumber", inline=True)
+		embedVar.add_field(name=str(self.land_ore), value="Land Ore", inline=True)
+		embedVar.add_field(name=str(self.land_uranium), value="Land Uranium", inline=True)
+		embedVar.add_field(name=str(self.land_fertility), value="Soil Fertility", inline=True)
+		embedVar.add_field(name=str(self.standard_of_living), value="Standard of Living", inline=True)
 		embedVar.add_field(name=str(self.nationalism), value="Nationalism", inline=True)
-		embedVar.add_field(name=str(self.natural_resources), value="Natural Resources", inline=True)
+		embedVar.add_field(name=str(self.education), value="Education", inline=True)
 		embedVar.add_field(name=str(self.wealth), value="Wealth", inline=True)
 		embedVar.add_field(name=str(self.income), value="Income", inline=True)
-		embedVar.add_field(name=str(self.army_size), value="Army Size", inline=True)
 		embedVar.add_field(name=str(self.population), value="Population", inline=True)
-		embedVar.add_field(name=str(self.combat_power), value="Combat Power", inline=True)
-		embedVar.add_field(name=str(self.unit_types), value="Unit Types", inline=True)
-		embedVar.add_field(name=str(self.infrastructure), value="Infrastructure", inline=True)
-		embedVar.add_field(name=str(self.tech), value="Tech", inline=True)
+		embedVar.add_field(name=str(self.lumber), value="Lumber", inline=True)
+		embedVar.add_field(name=str(self.ore), value="Ore", inline=True)
+		embedVar.add_field(name=str(self.uranium), value="Uranium", inline=True)
+		embedVar.add_field(name=str(self.food), value="Food", inline=True)
 		await ctx.send(embed=embedVar)
 
 
@@ -176,6 +235,7 @@ async def clear333(ctx):
 	embedVar.add_field(name="3- Categories", value="Clear all the categories", inline=False)
 	embedVar.add_field(name="4- Roles", value="Clear all the roles", inline=False)
 	embedVar.add_field(name="5- A country", value="Clear a specific country", inline=False)
+	embedVar.add_field(name="6- Empty channels", value="Clear random channels", inline=False)
 	await ctx.send(embed=embedVar)
 	def check(m):
 		return m.channel == ctx.message.channel and m.author == ctx.message.author
@@ -290,6 +350,35 @@ async def clear333(ctx):
 		else:
 			await ctx.send(f"{confirmC} not found. Please try again.")
 			return
+	elif clrOption == "6":
+		whitelist = ["mmun", "text channels", "chair"]
+		chnls = []
+		for i in ctx.guild.channels:
+			if i.name not in whitelist:
+				name = i.category
+				if name == None:
+					name = "None"
+					chnls.append(i)
+				else:
+					name = name.name
+				print(i.name + "- " + name)
+		if len(chnls) == 0:
+			return
+		else:
+			for i in chnls:
+				await ctx.send(i.name)
+			await ctx.send("Do you want to delete the above channels? (y/n)")
+			def check(m):
+				return m.channel == ctx.message.channel and m.author == ctx.message.author
+			confi = await client.wait_for('message', check=check)
+			if confi.content == "y":
+				for i in chnls:
+					await i.delete()
+				await ctx.send("Complete")
+			
+			
+				
+				
 		
 @client.command(aliases=["cc"])
 async def createCountry(ctx):
@@ -342,7 +431,20 @@ async def stats(ctx):
 	print(target)
 	if target != None:
 		await target.getStats(ctx)
-		
+
+@client.command(aliases=["t", "tech"])
+async def technologies(ctx):
+	target = None
+	with open('Countries', 'rb') as ctry:
+		Countries = pickle.load(ctry)
+	for i in Countries:
+		if ctx.message.author.id == i.managerID:
+			target = i
+			break
+	print(target)
+	if target != None:
+		await target.techTree(ctx)
+
 @client.command(aliases=["h"])
 async def help(ctx):
 	#no help
@@ -555,7 +657,8 @@ async def crisisOpt(ctx):
 	embed.add_field(name='React', value="f", inline=False)
 	embed.add_field(name='Reported By', value="g", inline=False)
 	await ctx.send(embed=embed)
-	
-	
+
+#token = "sugondese nutz"
+#save_object(token, "token.p")
 token = pickle.load(open("token.p", "rb"))
 client.run(token)
